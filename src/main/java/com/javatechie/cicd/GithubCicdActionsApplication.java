9137fa7049f7ba4c10067c57591fe8433d9bccef
RAJ <95871853+rajuvgit@users.noreply.github.com>
2022-08-05 12:25:26 +0530

    Changed Content

--- src/main/java/com/javatechie/cicd/GithubCicdActionsApplication.java
+++ src/main/java/com/javatechie/cicd/GithubCicdActionsApplication.java
@@ -11,7 +11,7 @@ public class GithubCicdActionsApplication {
 
 	@GetMapping("/welcome")
 	public String welcome(){
-		return "Welcome to javatechie !";
+		return " Successfully Build Spring-Boot Application & Pushed to Docker Hub  !";
 	}
 
 //	echo "# github-actions-example" >> README.md
